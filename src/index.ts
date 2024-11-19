d26edd412bab73307c199452c69e8d1f7dd465b4
Chaedong Im <chaedongim@igp.kr>
2024-11-19 12:14:23 +0900

    chore: add eslint, prettier

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,28 @@
-import { AxiosResponse } from "axios";
+import { AxiosResponse } from 'axios'
 
 export function axiosResponseToCurlCommand(response: AxiosResponse) {
-  const { config } = response;
-  const { method = "GET", baseURL = "", url, headers = {}, data } = config;
+  const { config } = response
+  const { method = 'GET', baseURL = '', url, headers = {}, data } = config
 
   // 전체 URL 구성 (baseURL + url)
-  const fullUrl = new URL(url ?? "", baseURL).toString();
+  const fullUrl = new URL(url ?? '', baseURL).toString()
 
   // 기본 curl 명령어 시작
-  let curlCommand = `curl -X ${method.toUpperCase()}/n`;
+  let curlCommand = `curl -X ${method.toUpperCase()}/n`
 
   // 헤더 추가
   Object.entries(headers).forEach(([key, value]) => {
-    curlCommand += ` -H "${key}: ${value}"/n`;
-  });
+    curlCommand += ` -H "${key}: ${value}"/n`
+  })
 
   // 데이터 추가 (GET 요청은 제외)
-  if (data && method.toUpperCase() !== "GET") {
-    const dataString = typeof data === "object" ? JSON.stringify(data) : data;
-    curlCommand += ` -d '${dataString}'/n`;
+  if (data && method.toUpperCase() !== 'GET') {
+    const dataString = typeof data === 'object' ? JSON.stringify(data) : data
+    curlCommand += ` -d '${dataString}'/n`
   }
 
   // URL 추가
-  curlCommand += ` "${fullUrl}"`;
+  curlCommand += ` "${fullUrl}"`
 
-  return curlCommand;
+  return curlCommand
 }
