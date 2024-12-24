2bdfc678e5b1a9d37b4e93afa236004be48d190f
ChaedongIm <chaedong.im.dev@gmail.com>
2024-12-24 10:56:02 +0900

    fix: type error

--- src/test.ts
+++ src/test.ts
@@ -2,7 +2,7 @@ import axios, { AxiosResponse } from 'axios'
 import { axiosResponseToCurlCommand } from './index'
 
 axios.interceptors.response.use(async (response: AxiosResponse<any>) => {
-  const curlCommand = axiosResponseToCurlCommand(response, response.config.url)
+  const curlCommand = axiosResponseToCurlCommand(response, response.config.url as string)
   console.debug('curlCommand = ', curlCommand)
 
   return response
